4258b4656e31f2cfbc64a712b82a8662fed5fc11
Alfonso Lobato Lopez <alobato1324@gmail.com>
2023-05-10 12:47:56 +0200

    Gateway y Filters

---
+++ Personal/src/main/java/com/nttdata/bootcamp/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.nttdata.bootcamp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	
+	
+	@GetMapping("/obtener")
+	public String personal() {
+		return "personal";
+	}
+
+}
